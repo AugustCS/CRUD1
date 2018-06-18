07841d6de31cde240941a5511c1b5e8a82ee272d
Carlos Cubas <cusan2010@gmail.com>
2018-06-18 14:12:35 -0500

    cambios jose
    cargando sus cambios

--- app/src/main/java/com/debugia/crudinicial/ConexionSQL.java
+++ app/src/main/java/com/debugia/crudinicial/ConexionSQL.java
@@ -1,41 +1,63 @@
 package com.debugia.crudinicial;
 
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.sql.*;
 
 public class ConexionSQL {
 
-
-    ArrayList nicks = new ArrayList<String>();
-
-    public Boolean VerificarIngreso(String RUC, String Usuario, String Clave) {
-/*
+    public static Connection ConnectionHelper() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
 
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("192.168.1.111\SQLSERVER2008R2\
-                    Bd_Consultoria_2015", "sa", "Solu123456
-                    ");
-
-                    String stsql = "select * from Hcliente where RUC='' and Usuario='' and Clave='' and Estado=''";
-
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(stsql);
-
-            while(rs.next()){
-                nicks.add(rs.getString("Name"));
-            }
-
-            conn.close();
-            if(nicks.Rows.count>0)
-                return true;
-
-        }catch (SQLException se) {
-            System.out.println("No se puede conectar. Error: " + se.toString());
+            ConnectionURL = "jdbc:jtds:sqlserver://192.168.1.111;port=1433;databaseName=Bd_Consultoria_2015;users=sa;password=Solu123456;";
+            connection = DriverManager.getConnection(ConnectionURL);
+        } catch (SQLException se) {
+            Log.e("ERROR", se.getMessage());
         } catch (ClassNotFoundException e) {
-            System.out.println("No se encuentra la clase. Error: " + e.getMessage());
+            Log.e("ERROR", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage());
         }
-        return false;
-        */
-        return false;
+        return connection;
     }
+
+    /*public boolean ObtenerDatos() {
+        String datosConsultado = "";
+        try {
+            //Se obtiene la conexión
+            Connection connect = ConexionSQL.ConnectionHelper();
+            //Se genera la consulta
+            Statement st = connect.createStatement();
+            ResultSet rs = st.executeQuery("select campo1,campo2 from tabla");
+            while (rs.next()) {
+                //Se extraen los datos
+                datosConsultado = rs.getString("campo1");
+            }
+            //Se cierra la conexión
+            connect.close();
+            //Mostramos los datos obtenidos
+            Toast.makeText(getApplicationContext(),
+                    datosConsultado, Toast.LENGTH_SHORT).show();
+            return  true;
+        } catch (SQLException e) {
+            //Mostramos el error en caso de no conectarse
+            Toast.makeText(getApplicationContext(),
+                    e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }*/
 }
