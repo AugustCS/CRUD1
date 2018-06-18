7a55cb6e0763e0c00e4be773c1d3c73d02f1a71d
José Cusimayta <jundower@gmail.com>
2018-06-18 15:35:06 -0500

    conexion exitosa sql server
    preparando codigo para listar

--- app/src/main/java/com/debugia/crudinicial/ConexionSQL.java
+++ app/src/main/java/com/debugia/crudinicial/ConexionSQL.java
@@ -2,59 +2,70 @@ package com.debugia.crudinicial;
 
 import android.os.StrictMode;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.sql.*;
 
 public class ConexionSQL {
+//192.168.1.111:1433/SQLSERVER2008R2
+//    String ip = "192.168.1.111\\SQLSERVER2008R2:1433";
+    String ip = "192.168.1.111:1433/SQLSERVER2008R2";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "Bd_Consultoria_2015";
+    String un = "sa";
+    String password = "Solu123456";
 
     public  Connection ConnectionHelper() {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection connection = null;
         String ConnectionURL = null;
 
         try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "jdbc:jtds:sqlserver://192.168.1.111;port=1433;databaseName=Bd_Consultoria_2015;users=sa;password=Solu123456;";
+            Class.forName(classs);
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
             connection = DriverManager.getConnection(ConnectionURL);
             Log.d("ConnectionHelper","Conexion EXITOSA");
         } catch (SQLException se) {
-            Log.e("ERROR", se.getMessage());
+            Log.e("ERROR SQLException", se.getMessage());
         } catch (ClassNotFoundException e) {
-            Log.e("ERROR", e.getMessage());
+            Log.e("ERROR ClassNotFound", e.getMessage());
         } catch (Exception e) {
-            Log.e("ERROR", e.getMessage());
+            Log.e("ERROR Exception", e.getMessage());
         }
         return connection;
     }
 
 
     public ArrayList getListFamilia() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
         ArrayList Familia = new ArrayList<String>();
         try{
-
+            //
+            //           jdbc:jtds:sqlserver://IP_PUBLICA:1433/baseDatos;instance=INSTANCIA_SQL;user=usuario;password=contraseña;");/
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            Connection conn = DriverManager
-                    .getConnection(
-                            "192.168.1.111\\SQLSERVER2008R2\\Bd_Consultoria_2015",
-                            "sa", "Solu123456");
+            ConnectionURL = "jdbc:jtds:sqlserver://192.168.1.111:1433/SQLSERVER2008R2;databaseName=Bd_Consultoria_2015;users=sa;password=Solu123456;";
+            connection = DriverManager.getConnection(ConnectionURL);
+
 
             String stsql = "select * from Hfam_art";
 
-            Statement st = conn.createStatement();
+            Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(stsql);
             while(rs.next()){
                 Familia.add(rs.getString(2));
             }
-            conn.close();
+            connection.close();
             Log.d("getListFamilia","exito");
 
         }catch (Exception e){
