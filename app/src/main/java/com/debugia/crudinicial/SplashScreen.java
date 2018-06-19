142aabc424f5b69fc679cc57e42960e678a8d350
Carlos Cubas <cusan2010@gmail.com>
2018-06-19 10:21:05 -0500

    Diseño Fragment SplashScreen
    Diseño Fragment SplashScreen

--- app/src/main/java/com/debugia/crudinicial/SplashScreen.java
+++ app/src/main/java/com/debugia/crudinicial/SplashScreen.java
@@ -1,6 +1,5 @@
 package com.debugia.crudinicial;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,47 +7,43 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class MenuPrincipal extends Fragment implements View.OnClickListener {
-    Button b_familia;
 
-    public MenuPrincipal() {
+public class SplashScreen extends Fragment {
+    public SplashScreen() {
         // Required empty public constructor
     }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_menu_principal, container, false);
-        b_familia= view.findViewById(R.id.b_familia);
-        b_familia.setOnClickListener(this);
-        return  view;
-    }
 
-    @Override
-    public void onClick(View v) {
-        Fragment fragment;
-        switch (v.getId()){
-            case (R.id.b_familia):
-                fragment= new familia();
-                CambiarFragment(fragment);
-                break;
-        }
+        final Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Fragment fragment = new Login();
+                    CambiarFragment(fragment);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //super.run();
+            }
+        };
+        thread.start();
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
     }
 
     public void CambiarFragment(Fragment fragment){
         FragmentManager fragmentManager = getFragmentManager();
+
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frag_contenedor, fragment);
-        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 }
